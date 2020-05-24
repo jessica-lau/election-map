b49690b5e0f4ddac5ea60cd5c32b4d5dded61482
jessica-lau <jessica.lau2@gmail.com>
2020-05-24 15:33:04 -0400

    added mouseover highlight by state for winner

--- js/election-map.js
+++ js/election-map.js
@@ -22,6 +22,9 @@ var createPolitician = function (name, color) {
 var politician1 = createPolitician("Sally Lee", [132, 17, 11]);
 var politician2 = createPolitician("Laura Wells", [245, 141, 136]);
 
+console.log("Sally Lee's party color is " + politician1.color);
+console.log("Laura Wells' party color is " + politician2.color);
+
 politician1.electionResults = [5, 1, 7, 2, 33, 6, 4, 2, 1, 14, 8, 3, 1, 11, 11, 0, 5, 3, 3, 3, 7, 4, 8, 9, 3, 7, 2, 2, 4, 2, 8, 3, 15, 15, 2, 12, 0, 4, 13, 1, 3, 2, 8, 21, 3, 2, 11, 1, 3, 7, 2];
 politician2.electionResults = [4, 2, 4, 4, 22, 3, 3, 1, 2, 15, 8, 1, 3, 9, 0, 6, 1, 5, 5, 1, 3, 7, 8, 1, 3, 3, 1, 3, 2, 2, 6, 2, 14, 0, 1, 6, 7, 3, 7, 3, 6, 1, 3, 17, 3, 1, 2, 11, 2, 3, 1];
 
@@ -45,11 +48,11 @@ var setStateResults = function (state) {
 
     theStates[state].winner = null;
 
-    if (politician1.voteResults[state] > politician2.voteResults[state]) {
+    if (politician1.electionResults[state] > politician2.electionResults[state]) {
 
         theStates[state].winner = politician1;
     }
-    else if (politician1.voteResults[state] < politician2.voteResults[state]) {
+    else if (politician1.electionResults[state] < politician2.electionResults[state]) {
 
         theStates[state].winner = politician2;
     }
@@ -62,7 +65,7 @@ var setStateResults = function (state) {
         theStates[state].rgbColor = stateWinner.color;
     }
     else {
-        theStates[state].rbgColor = [11, 32, 57];
+        theStates[state].rgbColor = [11, 32, 57];
     }
 
 };
@@ -81,8 +84,8 @@ else {
 
 console.log("The winner is " + winner + "!");
 
-console.log("Sally Lee's party color is " + politician1.color);
-console.log("Laura Wells' party color is " + politician2.color);
+
+
 
 
 
